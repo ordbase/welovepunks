0249438bd7ab78612d24e83027469a6388ac7f70
Gerald Bauer <gerald.bauer@gmail.com>
2024-03-13 15:00:56 +0100

    up

---
+++ generate_aliens.rb
@@ -0,0 +1,43 @@
+require 'pixelart'
+
+
+require_relative 'base'
+
+
+## note: small shades same as regular shades for now
+
+specs = parse_data( <<DATA )
+ alien, bandana, regular shades 
+ alien, cap
+ alien, headband
+ alien, cowboy hat, earring
+ alien, bandana
+ alien, do-rag, regular shades   
+ alien, knitted cap, earring
+ alien, knitted cap, medical mask, earring
+ alien, cap forward, regular shades, pipe
+DATA
+
+
+
+cols = 3
+rows = specs.size / cols 
+rows += 1    if specs.size % cols != 0
+
+composite = ImageComposite.new( cols, rows, 
+                                  width: 24, height: 24 )
+
+
+specs.each_with_index do |spec, i| 
+     img = generate( *spec)
+     
+     img.save( "./tmp/alien#{i}.png" )
+     img.zoom(10).save( "./tmp/alien#{i}@10x.png" )
+     composite << img
+end
+
+
+composite.save( "./tmp/aliens.png" )
+composite.zoom(4).save( "./tmp/aliens@4x.png" )
+
+puts "bye"
